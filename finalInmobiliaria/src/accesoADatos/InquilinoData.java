6f31448b760010d9151428cd12b6a89a652dbb92
lucianoidiart <141642491+lucianoidiart@users.noreply.github.com>
2023-10-06 10:27:11 -0300

    inquilinoData y propiedadData

--- finalInmobiliaria/src/accesoADatos/InquilinoData.java
+++ finalInmobiliaria/src/accesoADatos/InquilinoData.java
@@ -1,14 +1,183 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package accesoADatos;
 
-/**
- *
- * @author ULP
- */
+import Entidades.Inquilino;
+import Entidades.Propietario;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
+
+
 public class InquilinoData {
+     private Connection con = null;
     
+    public InquilinoData(){
+        
+        con = miConexion.getConexion();//me conecto a la base
+             
+    }
+    
+     public void guardarInquilino(Inquilino inquilino) {
+
+        String sql = " INSERT INTO `inquilino`(`apellido`, `nombre`, `DNI`, `detalle`, `tipo`,`cuit`,`telefono`,`estado`)VALUES (?,?,?,?,?,?,?,?)";
+
+        try {
+            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, inquilino.getApellido());  
+            ps.setString(2, inquilino.getNombre());
+            ps.setInt(3, inquilino.getDni());
+            ps.setString (4, String.valueOf(inquilino.getDetalle()));
+            ps.setString(5, String.valueOf(inquilino.getTipo()));
+            ps.setString(6,inquilino.getCuit());
+            ps.setString(7, inquilino.getTelefono());
+            ps.setBoolean(8, true);
+            
+            ps.executeUpdate();
+            ResultSet rs = ps.getGeneratedKeys();
+
+            if (rs.next()) {
+
+               inquilino.setId_inquilino(rs.getInt(1));
+                JOptionPane.showMessageDialog(null, "inquilino Guardado");
+
+            }
+
+            ps.close();
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al Acceder a la tabla inquilino");
+        }
+
+    }
+    
+    public void modificarInquilino(Inquilino inquilino) {
+
+        String sql = "UPDATE `inquilino` SET `apellido`=?,`nombre`=?,`DNI`=?,`detalle`=?,`tipo`=?,`cuit`=?,`telefono`=? WHERE id_Inquilino=?";
+        PreparedStatement ps = null;
+
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setString(1, inquilino.getApellido());  
+            ps.setString(2, inquilino.getNombre());
+            ps.setInt(3, inquilino.getDni());
+            ps.setString(4, String.valueOf(inquilino.getDetalle()));
+            ps.setString(5,String.valueOf(inquilino.getTipo()));
+            ps.setString(6, inquilino.getCuit());
+            ps.setString(7, inquilino.getTelefono());
+            
+            int exito = ps.executeUpdate();
+
+            if (exito == 1) {
+                JOptionPane.showMessageDialog(null, "Propietario Modificado Exitosamente.");
+            } else {
+                JOptionPane.showMessageDialog(null, "El Propietario no existe");
+            }
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Propietario " + ex.getMessage());
+        }
+
+    }
+     
+      public Inquilino buscarInquilinoPorID(int id) {
+        Inquilino inquilino=null;
+        
+         
+        String sql = "SELECT apellido, nombre, DNI, domicilio, telefono FROM inquilino WHERE id_Inquilino = ? AND estado = 1";
+        PreparedStatement ps = null;
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                inquilino = new Inquilino();
+                inquilino.setId_inquilino(id);
+                inquilino.setApellido(rs.getString("apellido"));
+                inquilino.setNombre(rs.getString("nombre"));
+                inquilino.setDni(rs.getInt("DNI"));
+                String deta=rs.getString("detalle");
+                char detalle1=deta.charAt(0);
+                inquilino.setDetalle(detalle1);
+                String tip=rs.getString("tipo");
+                char tipo1=tip.charAt(0);
+                inquilino.setTipo(tipo1);
+                inquilino.setCuit(rs.getString("cuit"));
+                inquilino.setTelefono(rs.getString("telefono"));
+                inquilino.setEstado(true);
+                                     
+               
+
+            } else {
+                JOptionPane.showMessageDialog(null, "No existe el Inquilino");
+                ps.close();
+            }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla inquilino" + ex.getMessage());
+
+        }
+
+        return inquilino;
+    }
+     
+     
+      
+    public List<Inquilino> listarinquilino() {
+
+        List<Inquilino> inquilinos = new ArrayList<>();
+        try {
+            String sql = "SELECT * FROM inquilino WHERE estado = 1 ";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+               Inquilino inquilino;
+               
+                inquilino = new Inquilino();
+                inquilino.setId_inquilino(rs.getInt("id_Inquilino"));
+                inquilino.setApellido(rs.getString("apellido"));
+                inquilino.setNombre(rs.getString("nombre"));
+                inquilino.setDni(rs.getInt("DNI"));
+                String deta=rs.getString("detalle");
+                char detalle1=deta.charAt(0);
+                inquilino.setDetalle(detalle1);
+                String tip=rs.getString("tipo");
+                char tipo1=tip.charAt(0);
+                inquilino.setTipo(tipo1);
+                inquilino.setCuit(rs.getString("cuit"));
+                inquilino.setTelefono(rs.getString("telefono"));
+                inquilino.setEstado(true);
+
+                inquilinos.add(inquilino);
+                
+                
+            }
+            ps.close();
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, " Error al acceder a la tabla Alumno " + ex.getMessage());
+        }
+        return inquilinos;
+    }
+    
+    
+    public void eliminarInquilino(int id) {
+        try {
+            String sql = "UPDATE inquilino SET estado = 0 WHERE id_Inquilino = ? ";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, id);
+            int fila = ps.executeUpdate();
+            if (fila == 1) {
+                JOptionPane.showMessageDialog(null, " Se eliminó el Inquilino.");
+            }
+            ps.close();
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, " Error al acceder a la tabla inquilino");
+        }
+
+    }
 }
--- finalInmobiliaria/src/accesoADatos/PropiedadData.java
+++ finalInmobiliaria/src/accesoADatos/PropiedadData.java
@@ -1,14 +1,190 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package accesoADatos;
 
-/**
- *
- * @author ULP
- */
+import Entidades.Propiedad;
+import Entidades.Propietario;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
+
+
 public class PropiedadData {
+    
+     private Connection con = null;
+     private PropietarioData pd=new PropietarioData();
+    public PropiedadData(){
+        
+          con = miConexion.getConexion();//me conecto a la base
+         
+        
+    }
+
+    
+    public void guardarPropiedad(Propiedad propiedad) {
+
+        String sql = " INSERT INTO `propiedadinmueble`(`id_Propietario`, `accesibilidad`, `direccion`, `forma`, `precioTazado`, `revisor`, `superficieMinima`, `tipoDeLocal`, `zona`, `estado`)VALUES (?,?,?,?,?,?,?,?,?,?)";
+
+        try {
+            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, propiedad.getPropietario().getId_propietario());  
+            ps.setString(2, propiedad.getAccesibilidad());
+            ps.setString(3, propiedad.getDireccion());
+            ps.setString(4, propiedad.getForma());
+            ps.setFloat(5, propiedad.getPrecio());
+            ps.setString(6,propiedad.getRevisor());
+            ps.setInt(7,propiedad.getSuperficieMinima());
+            ps.setString(8,propiedad.getTipoPropiedad());
+            ps.setString(9,propiedad.getZona());
+            ps.setString(10,null);
+            
+            
+            ps.executeUpdate();
+            ResultSet rs = ps.getGeneratedKeys();
+
+            if (rs.next()) {
+
+               propiedad.setId_propiedad(rs.getInt(1));
+                JOptionPane.showMessageDialog(null, "propiedad Guardada");
+
+            }
+
+            ps.close();
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al Acceder a la tabla propiedad");
+        }
+
+    }
+    
+    public void modificarPropiedad(Propiedad propiedad) {
+
+        String sql = "UPDATE `propiedadinmueble` SET `id_Propietario`=?,`accesibilidad`=?,`direccion`=?,`forma`=?,`precioTazado`=? ,`revisor`=? ,`superficieMinima`=?,`tipoDeLocal`=?,`zona`=? WHERE id_Propiedad=?";
+        PreparedStatement ps = null;
+
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, propiedad.getPropietario().getId_propietario());  
+            ps.setString(2, propiedad.getAccesibilidad());
+            ps.setString(3, propiedad.getDireccion());
+            ps.setString(4, propiedad.getForma());
+            ps.setFloat(5, propiedad.getPrecio());
+            ps.setString(6,propiedad.getRevisor());
+            ps.setInt(7,propiedad.getSuperficieMinima());
+            ps.setString(8,propiedad.getTipoPropiedad());
+            ps.setString(9,propiedad.getZona());
+            ps.setInt(10,propiedad.getId_propiedad()) ;
+            int exito = ps.executeUpdate();
+
+            if (exito == 1) {
+                JOptionPane.showMessageDialog(null, "Propidad Modificada Exitosamente.");
+            } else {
+                JOptionPane.showMessageDialog(null, "La propiedad no existe");
+            }
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Propiedad " + ex.getMessage());
+        }
+
+    }
+     
+      public Propiedad buscarPropiedadPorID(int id) {
+        Propiedad propiedad = null;
+        String sql = "SELECT id_propietario,accesibilidad,direccion,forma,precioTazado,revisor,superficieMinima,tipoDeLocal,zona FROM propiedad WHERE id_Propiedad = ? AND estado = 1";
+        PreparedStatement ps = null;
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                propiedad = new Propiedad();
+                propiedad.setId_propiedad(id); 
+                Propietario propie=pd.buscarPropietarioPorID(rs.getInt("id_propietario"));
+                
+                propiedad.setPropietario(propie);
+                propiedad.setAccesibilidad(rs.getString("accesibilidad"));
+                propiedad.setDireccion(rs.getString("direccion"));
+                propiedad.setForma(rs.getString("forma"));
+                propiedad.setPrecio(rs.getInt("precioTazado"));
+                propiedad.setRevisor(rs.getString("revisor"));
+                propiedad.setSuperficieMinima(rs.getInt("superficieMinima"));
+                propiedad.setTipoPropiedad(rs.getString("tipoDeLocal"));
+                propiedad.setZona(rs.getString("zona"));
+                                
+                          
+
+            } else {
+                JOptionPane.showMessageDialog(null, "No existe la propiedad");
+                ps.close();
+            }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla propiedad " + ex.getMessage());
+
+        }
+
+        return propiedad;
+        
+        
+    }
+     
+     
+      
+    public List<Propiedad> listarPropiedad() {
+
+        List<Propiedad> propiedades = new ArrayList<>();
+        try {
+            String sql = "SELECT * FROM propiedad WHERE estado = 1 ";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+               Propiedad propiedad;
+               
+                propiedad = new Propiedad();
+                Propietario propie=pd.buscarPropietarioPorID(rs.getInt("id_propietario"));
+                
+                propiedad.setPropietario(propie);
+                propiedad.setAccesibilidad(rs.getString("accesibilidad"));
+                propiedad.setDireccion(rs.getString("direccion"));
+                propiedad.setForma(rs.getString("forma"));
+                propiedad.setPrecio(rs.getInt("precioTazado"));
+                propiedad.setRevisor(rs.getString("revisor"));
+                propiedad.setSuperficieMinima(rs.getInt("superficieMinima"));
+                propiedad.setTipoPropiedad(rs.getString("tipoDeLocal"));
+                propiedad.setZona(rs.getString("zona"));
+                
+                
+                propiedades.add(propiedad);
+                
+                
+            }
+            ps.close();
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, " Error al acceder a la tabla propiedad" + ex.getMessage());
+        }
+        return propiedades;
+    }
+    
+    
+    public void eliminarPropiedad(int id) {
+        try {
+            String sql = "UPDATE propiedadinmueble SET estado = 0 WHERE id_Propiedad = ? ";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, id);
+            int fila = ps.executeUpdate();
+            if (fila == 1) {
+                JOptionPane.showMessageDialog(null, " Se eliminó la propiedad.");
+            }
+            ps.close();
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, " Error al acceder a la tabla Propiedad");
+        }
+
+    }
     
 }
